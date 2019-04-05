fddaa2a23c95a9ced6a95ea05a2e52700c4e3104
CaseyVBox <crhird@g.clemson.edu>
2019-04-05 16:27:57 -0400

    add test log

---
+++ mp4_final/test_cases.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# test_cases.sh
+# Casey Hird
+# crhird
+# ECE 2230 Spring 2019
+# MP4
+
+# Script to run test cases
+
+
+# Iterate through the following sort orders
+sortOrders='best first worst'
+# Test these unit test drivers
+unitTests='0 1 2 3 4 5 6'
+# Use the following random number generator seed
+seeds='19283 8234'
+
+
+# Run each case without coalescing
+for u in $unitTests
+do
+	for s in $sortOrders
+	do
+		for r in $seeds
+		do
+			./lab4 -s $s -f $s -u $u
+		done
+	done
+done
+
+# Run each case with coalescing
+for u in $unitTests
+do
+	for s in $sortOrders
+	do
+		for r in $seeds
+		do
+			./lab4 -s $s -f $s -c -u $u
+		done
+	done
+done
+
